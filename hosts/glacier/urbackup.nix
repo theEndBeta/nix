d817834aecd87b6f8129eb6e0f71406acd34c822
Aidan Stein <astein@vesulabs.us>
2023-09-26 20:34:57 -0400

    urbackup suid

--- hosts/glacier/urbackup.nix
+++ hosts/glacier/urbackup.nix
@@ -6,10 +6,28 @@
   ];
 
   users = {
+    groups = {
+      urbackup = {};
+    };
     users.urbackup = {
       isSystemUser = true;
       group = "urbackup";
       createHome = false;
+    };
+
+    security.wrappers = {
+      urbackup_snapshot_helper = {
+        setuid = true;
+        owner = "urbackup";
+        group = "urbackup";
+        source = "${pkgs.urbackup}/urbackup_snapshot_helper";
+      };
+      urbackup_mount_helper = {
+        setuid = true;
+        owner = "urbackup";
+        group = "urbackup";
+        source = "${pkgs.urbackup}/urbackup_snapshot_helper";
+      };
     };
   };
 }
