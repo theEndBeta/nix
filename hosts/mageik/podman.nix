7f41ba8314394ec6d17bb0595b4c3371c2047464
Aidan Stein <astein@vesulabs.us>
2023-12-30 14:51:59 -0500

    use podman from unstable channel

--- hosts/mageik/podman.nix
+++ hosts/mageik/podman.nix
@@ -1,13 +1,20 @@
-{ config, lib, pkgs, modulesPath, ... }:
+{ config, lib, pkgs, pkgs-unstable, modulesPath, ... }:
 
+let
+  podmanModule = "virtualisation/podman/default.nix";
+in
 {
+  # swap to podman from unstable
+  disabledModules = [ podmanModule ];
+  imports = [ "<nixos-unstable/nixos/modules/${podmanModule}>" ];
+
   virtualisation.podman = {
     enable = true;
     autoPrune.enable = true;
   };
 
   environment.systemPackages = [
-    pkgs.podman-tui
+    pkgs-unstable.podman-tui
   ];
 
 }
