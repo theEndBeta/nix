ca709b4e571362ec270f6169119f865ccf8edec9
Aidan Stein <astein@vesulabs.us>
2024-05-04 14:58:04 -0400

    add tailscale to glacier

--- hosts/glacier/configuration.nix
+++ hosts/glacier/configuration.nix
@@ -12,6 +12,14 @@
       ./urbackup.nix
     ];
 
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It's perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+
   time.timeZone = "America/New_York";
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
@@ -25,48 +33,18 @@
     zfs.forceImportRoot = false;
     zfs.extraPools = [ "dpool" ];
   };
-
-  networking.hostId = "3792e26b"; # `head -c 8 /etc/machine-id`
-
-  networking.hostName = "glacier"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
-
-  # Set your time zone.
-  # time.timeZone = "Europe/Amsterdam";
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkbOptions in tty.
-  # };
-
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-
-
   
+  networking = {
+    hostId = "3792e26b"; # `head -c 8 /etc/machine-id`
 
-  # Configure keymap in X11
-  # services.xserver.layout = "us";
-  # services.xserver.xkbOptions = "eurosign:e,caps:escape";
+    hostName = "glacier"; # Define your hostname.
 
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # sound.enable = true;
-  # hardware.pulseaudio.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [ 22 ];
+      # allowedUDPPorts = [ ... ];
+    };
+  };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users = {
@@ -148,36 +126,20 @@
   #   enableSSHSupport = true;
   # };
 
-  # List services that you want to enable:
-
   # Enable the OpenSSH daemon.
   services.openssh = {
     enable = true;
     settings.PasswordAuthentication = true;
-    settings.PermitRootLogin = "yes";   
+    settings.PermitRootLogin = "yes";
   };
-
-  # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 22 ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It's perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
 
   services.zfs = {
     autoScrub.enable = true;
+  };
+
+  services.tailscale = {
+    enable = true;
+    extraUpFlags = [ "-ssh" ];
   };
 }
 
