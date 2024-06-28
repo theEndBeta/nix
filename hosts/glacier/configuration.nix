84b87a713502bb98279b3214377c13b5d3129c72
Aidan Stein <astein@vesulabs.us>
2024-06-27 20:52:41 -0400

    glacier,mageik - auto nix garabage collect

--- hosts/glacier/configuration.nix
+++ hosts/glacier/configuration.nix
@@ -24,6 +24,12 @@
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
+
   # Use the systemd-boot EFI boot loader.
   boot = {
     loader.systemd-boot.enable = true;
