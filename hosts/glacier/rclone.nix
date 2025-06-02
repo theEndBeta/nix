c75ce8e675196b35b9b9e5e1707154a8a245e962
Aidan Stein <astein@vesulabs.us>
2025-06-01 20:28:34 -0400

    add systemd service for ntfy-vaultwarden backup
    host: glacier

--- hosts/glacier/rclone.nix
+++ hosts/glacier/rclone.nix
@@ -3,4 +3,33 @@
   environment.systemPackages = [pkgs.rclone];
 
   environment.etc."rclone.conf".source = ./rclone.conf;
+
+
+  systemd.user.services.vaultwarden-db-backup = {
+    unitConfig.ConditionUser = "backup|vesu";
+    requires = ["mnt-dpool-backups-vaultwarden.mount"];
+    wantedBy = ["default.target"];
+    path = [pkgs.nix];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = [
+        "tailscale ssh vesu@ntfy -- systemctl --user start vaultwarden-db-backup"
+        "rclone sync \
+          --config /etc/rclone.conf \
+          --progress \
+          --fix-case \
+          --track-renames \
+          --delete-excluded \
+          --filter '+ *.backup.sqlite3' \
+          --filter '+ config.json' \
+          --filter '+ rsa_key*' \
+          --filter '+ attachments/**' \
+          --filter '+ backups/**' \
+          --filter '- *' \
+          ntfy:/data/appdata/vaultwarden \
+          /mnt/dpool/backups/vaultwarden"
+        "zfs snapshot dpool/backups/vaultwarden@$(date +%Y%m%d)"
+      ];
+    };
+  };
 }
