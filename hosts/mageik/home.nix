ac349fc3f939b98939038ef20a50692965382909
Aidan Stein <astein@vesulabs.us>
2026-02-04 18:30:41 -0500

    update(mageik) upstream git settings reorganization

--- hosts/mageik/home.nix
+++ hosts/mageik/home.nix
@@ -9,9 +9,11 @@
       programs = {
         git = {
           enable = true;
-          userName = "vesu@mageik";
-          userEmail = "2302239+theEndBeta@users.noreply.github.com.";
-          extraConfig = {
+          settings = {
+            user= {
+              name = "vesu@mageik";
+              email = "2302239+theEndBeta@users.noreply.github.com.";
+            };
             commit.gpgsign = true;
             gpg.format = "ssh";
             user.signingkey = "~/.ssh/id_ed25519.pub";
